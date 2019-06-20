1afe0431dccd4d1066bc98a35436288f8c3bf0f2
andaje <andamad72@yahoo.com>
2019-06-20 15:56:05 +0200

    last last commit

--- database/factories/BookFactory.php
+++ database/factories/BookFactory.php
@@ -8,12 +8,12 @@ use Faker\Generator as Faker;
 $factory->define(Book::class, function (Faker $faker) {
     return [
         //
-        'author_id'=> $faker->randomNumber(2),
+        'author_id'=> $faker->numberBetween(1,9),
         'title' => implode(' ', $faker->words(3)),
         'isbn'=> implode(' ', $faker->words(3)),
         'edition'=> implode(' ', $faker->words(3)),
         'year'=> 1999,
-        'photo_id'=> $faker->randomNumber(2),
+        'photo_id'=> $faker->numberBetween(1,9),
         'description'=> implode(' ', $faker->words(10))
 
     ];
